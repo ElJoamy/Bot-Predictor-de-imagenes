946caf99416c934924206453b15dab28442bda80
ElJoamy <josephsalguero1@upb.edu>
2023-11-21 05:08:51 -0400

    Add config.py with initial settings

---
+++ config.py
@@ -0,0 +1,17 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from functools import cache
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file='.env')
+    
+    api_name: str = "Object Detection service"
+    revision: str = "local"
+    yolo_version: str = "yolov8x.pt"
+    log_level: str = "DEBUG"
+    telegram_token: str
+
+
+@cache
+def get_settings():
+    print("getting settings...")
+    return Settings()
